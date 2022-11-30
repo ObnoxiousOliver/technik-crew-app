48d03f8059b1e4ae97132aff23440899c3f9fdd0
ObnoxiousOliver <maxhd2005@gmail.com>
2022-11-30 17:12:59 +0100

    setup and placeholder website

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,61 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <h2>
+    Technik Crew App<br>
+    Development started since
+  </h2>
+  <h3>
+    {{ days }} Days<br>
+    {{ hours }} Hours<br>
+    {{ minutes }} Minutes<br>
+    {{ seconds }} Seconds<br>
+    {{ miliseconds }} Miliseconds
+  </h3>
 </template>
 
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const time = ref('0')
+const days = ref()
+const hours = ref()
+const minutes = ref()
+const seconds = ref()
+const miliseconds = ref()
+
+const start = 1669823168271
+
+update()
+function update () {
+  const elapsed = Date.now() - start
+  days.value = Math.floor(elapsed / (24 * 60 * 60 * 1000))
+  hours.value = Math.floor(elapsed / (60 * 60 * 1000)) % 24
+  minutes.value = Math.floor(elapsed / (60 * 1000)) % 60
+  seconds.value = Math.floor(elapsed / 1000) % 60
+  miliseconds.value = elapsed % 1000
+}
+setInterval(update)
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;600&display=swap');
+
+body {
+  background: black;
+  color: white;
+  font-family: Poppins, sans-serif;
   text-align: center;
-  color: #2c3e50;
+  height: 100vh;
+  margin: 0;
+  display: grid;
+  place-items: center;
+  padding-bottom: 5rem;
+  box-sizing: border-box;
 }
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+h2 {
+  font-weight: 300;
+}
+h3 {
+  font-weight: 600;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,23 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyDKTBOLLL7-L_OFFnQ1cJtyqwG2iAQn_UA',
+  authDomain: 'leibniz-technik-crew.firebaseapp.com',
+  projectId: 'leibniz-technik-crew',
+  storageBucket: 'leibniz-technik-crew.appspot.com',
+  messagingSenderId: '942452136166',
+  appId: '1:942452136166:web:e87215a8a6ae2620f4c327'
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+
+// Create and mount app
 createApp(App).use(router).mount('#app')
