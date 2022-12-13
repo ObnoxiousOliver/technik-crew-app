da6e69dd0779706b0335e60ac9531b95b63b02c2
ObnoxiousOliver <maxhd2005@gmail.com>
2022-12-13 18:41:09 +0100

    feat: Event Calendar with rough styling

---
+++ src/components/AddButton.vue
@@ -0,0 +1,13 @@
+<template>
+  <Btn class="add-btn">
+    <i class="bi-plus-lg" />
+  </Btn>
+</template>
+
+<style lang="scss" scoped>
+.add-btn {
+  padding: 0;
+  width: 3.125rem;
+  height: 3.125rem;
+}
+</style>
