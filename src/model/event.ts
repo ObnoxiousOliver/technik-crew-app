2552e08217c2255763dc180626bb264a9a8dab73
ObnoxiousOliver <maxhd2005@gmail.com>
2023-01-27 23:04:57 +0100

    feat: calendar in dashboard

--- src/model/event.ts
+++ src/model/event.ts
@@ -1,43 +1,101 @@
-import { createId } from '@/utilities/id'
+import { collection, doc, getDoc, getDocs, getFirestore, query } from 'firebase/firestore'
+
+export type EventColors = 'gray' | 'red' | 'orange' | 'yellow' | 'green' | 'cyan' | 'blue' | 'purple' | 'pink'
 
 export interface EventDB {
-  id: string,
   name: string,
   description: string,
   wholeDay: boolean,
   startDate: number,
   endDate: number | null,
-  color: 'gray' | 'red' | 'orange' | 'yellow' | 'green' | 'cyan' | 'blue' | 'purple' | 'pink'
+  color: EventColors
 }
 
-export default class Event implements EventDB {
+export default class Event {
   id: string
-  name: string
-  description: string
-  wholeDay: boolean
-  startDate: number
-  endDate: number | null
-  color: 'gray' | 'red' | 'orange' | 'yellow' | 'green' | 'cyan' | 'blue' | 'purple' | 'pink'
+  private event: EventDB
 
-  constructor (options: Partial<EventDB> = {}) {
-    this.id = options.id ?? createId()
-    this.name = options.name ?? 'Unbenannter Termin'
-    this.description = options.description ?? ''
-    this.wholeDay = options.wholeDay ?? true
-    this.startDate = options.startDate ?? new Date().getTime()
-    this.endDate = options.endDate ?? null
-    this.color = options.color ?? 'gray'
+  get name (): string {
+    return this.event.name
+  }
+
+  set name (name: string) {
+    this.event.name = name
+  }
+
+  get description (): string {
+    return this.event.description
+  }
+
+  set description (description: string) {
+    this.event.description = description
+  }
+
+  get wholeDay (): boolean {
+    return this.event.wholeDay
+  }
+
+  set wholeDay (wholeDay: boolean) {
+    this.event.wholeDay = wholeDay
+  }
+
+  get startDate (): number {
+    return this.event.startDate
+  }
+
+  set startDate (startDate: number) {
+    this.event.startDate = startDate
+  }
+
+  get endDate (): number | null {
+    return this.event.endDate
+  }
+
+  set endDate (endDate: number | null) {
+    this.event.endDate = endDate
+  }
+
+  get color (): EventColors {
+    return this.event.color
+  }
+
+  set color (color: EventColors) {
+    this.event.color = color
+  }
+
+  constructor (id: string, options: Partial<EventDB> = {}) {
+    this.id = id
+
+    this.event = {
+      name: options.name ?? 'Unbenannter Termin',
+      description: options.description ?? '',
+      wholeDay: options.wholeDay ?? true,
+      startDate: options.startDate ?? new Date().getTime(),
+      endDate: options.endDate ?? null,
+      color: options.color ?? 'gray'
+    }
   }
 
   toDB (): EventDB {
-    return {
-      id: this.id,
-      name: this.name,
-      description: this.description,
-      wholeDay: this.wholeDay,
-      startDate: this.startDate,
-      endDate: this.endDate,
-      color: this.color
+    return this.event
+  }
+
+  static async get (id?: string) {
+    const db = getFirestore()
+    if (id) {
+      const event = await getDoc(doc(db, 'events', id))
+      if (event.exists()) {
+        return new Event(id, event.data() as EventDB)
+      } else {
+        return null
+      }
+    } else {
+      const querySnapshot = (await getDocs(query(collection(db, 'events'))))
+      const events: Event[] = []
+      querySnapshot.forEach((doc) => {
+        events.push(new Event(doc.id, doc.data() as EventDB))
+      })
+      return events
     }
   }
 }
--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -4,15 +4,29 @@
       <i class="bi-house-door"/>Dashboard
     </template>
 
-    <DashboardCard>
-      <template #title>
-        <i class="bi-compass"/> asdasd
+    <EventCalendar :events="events">
+      <template #btns>
+        <Btn @click="refresh">
+          <i class="bi-arrow-counterclockwise"/>
+        </Btn>
       </template>
-    </DashboardCard>
+    </EventCalendar>
   </UserPage>
 </template>
 
 <script lang="ts" setup>
-import DashboardCard from '../components/DashboardCard.vue'
+import Event from '@/model/event'
+import { onMounted, ref } from 'vue'
+import EventCalendar from '../components/EventCalendar.vue'
 import UserPage from '../layout/UserPage.vue'
+
+const events = ref<Event[]>([])
+
+onMounted(async () => {
+  events.value = await Event.get()
+})
+
+async function refresh () {
+  events.value = await Event.get()
+}
 </script>
